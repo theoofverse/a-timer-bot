ac21112fef8b8726fdd6a3a0284e99804e8dd5cc
theoofverse <61157142+theoofverse@users.noreply.github.com>
2020-02-23 17:20:36 +0000

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ bot.on('message', message=>{
             message.channel.sendMessage('WHAT? ~~I THOUGHT WE ALWAYS DID!~~ THAT IS WEIRD!')
             break;
         case 'randomNumber':
-            let result = Math.floor((Math.random() * replies.length))
+            var result = Math.floor((Math.random() * replies.length))
             message.channel.send(array[result])
         break;
             
